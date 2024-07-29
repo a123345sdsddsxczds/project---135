5cbcbc76d925cd25d1961c63fbd894aae7ee00f3
a123345sdsddsxczds <143101152+a123345sdsddsxczds@users.noreply.github.com>
2024-07-29 09:43:06 +0530

    Add files via upload

--- mine.js
+++ mine.js
@@ -26,5 +26,26 @@ function modelLoaded() {
 }
 
 function draw() {
-    image(video, 0, 0, 480, 380);
+  image(video, 0, 0, 480, 380);
+  if (status != "") {
+    objectDetector.Detect(video, gotResult);
+    for (i = 0; i < object.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Detecting Objects";
+      document.getElementById("status").innerHTML = "Number of objects detected are : ";+ object.length;
+
+      fill("#FF0000")
+      percent = floor(objects[i].confidence * 100)
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15)
+      nofill()
+      stroke("#FF0000")
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
   }
+}
+
+function gotResult(error, result) {
+  if (error) {
+    console.log(error)
+  }
+  console.log(result)
+}
